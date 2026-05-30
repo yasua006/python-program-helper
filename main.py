ef8013b7e5ed544319d5c7cb4c421ba1531e9ac0
yasua006 <yasua006@osloskolen.no>
2026-05-30 20:03:46 +0200

    feat(legg til vanlig flask mapper): legg til og bruk uv istedenfor

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import atexit
 
 from modules.frontend_template import *
-from modules.empty_folders import ask_common_vanilla_frontend
+from modules.empty_folders import ask_common_vanilla_frontend, ask_common_flask_folders
 
 
 def main() -> None:
     ask_frontend_template()
     ask_common_vanilla_frontend()
+    ask_common_flask_folders()
 
 
 if __name__ == '__main__':
